ae2714f8e7defc7774d8e0c8b25d91739331a5bd
Neko19o7 <vanb1910481@student.ctu.edu.vn>
2022-10-26 14:36:52 +0700

    Tao trang cap nhat lien he

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script>
 import AppHeader from "@/components/AppHeader.vue";
-
 export default {
   components: {
     AppHeader,
   },
 };
 </script>
-
 <template>
   <div id="app">
     <AppHeader />
@@ -16,7 +14,6 @@ export default {
     </div>
   </div>
 </template>
-
 <style>
 .page {
   max-width: 400px;
